7bb52e4fa3ba7cbf31d6d1eb8d544a37cf4fd03a
alexandra-doris <doris.ciorba@gmail.com>
2021-04-23 13:21:29 +0300

    Add username, email unique validator

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,20 +1,23 @@
 import mongoose,{ Schema } from 'mongoose';
 import logging from '../config/logging';
 import IUser from '../interfaces/user';
+var uniqueValidator = require('mongoose-unique-validator');
 
 const UserSchema: Schema = new Schema(
     {
-        username: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+        username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
         password: {type: String, required: [true, "can't be blank"]},
         salt: {type: String},
         hash: {type: String},
-        email: {type: String, lowercase: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+        email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
         score: {type: Number}
     },
     {
         timestamps: true
     }
 );
+
+UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
 
 UserSchema.post<IUser>('save', function () {
     this.score = 0;
